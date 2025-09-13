23b0125c177241e01dfd394ce811838356305227
tangelraj <tangelraj@gmail.com>
2025-09-13 18:39:22 +0530

    Update settings.py

--- driverp_project/settings.py
+++ driverp_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#$r$3x!b5vzg30u2^27d!l!x(c@txmykwog45ma1mmjoq2vjnd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,3 +125,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
